a4dea969380700fbfbce6fb83f8b771d73d4d8e0
mohamed-samy2499 <1701198@eng.asu.edu.eg>
2022-06-27 15:14:37 +0200

    recipe list added

--- shopping-recipes-app/src/app/recipe/recipe-list/recipe-list.component.ts
+++ shopping-recipes-app/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[] = [new Recipe('A test recipe','this is test description'
+  ,'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F8963973.jpg'),
+  new Recipe('A test recipe','this is test description'
+  ,'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F8963973.jpg')];
   constructor() { }
 
   ngOnInit(): void {
