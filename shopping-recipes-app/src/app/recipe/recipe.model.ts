fed146ecd2abbcaf82c4139410245b7e461fcd7a
mohamed-samy2499 <1701198@eng.asu.edu.eg>
2022-07-01 15:38:34 +0200

    adding list element by custom property binding

--- shopping-recipes-app/src/app/recipe/recipe.model.ts
+++ shopping-recipes-app/src/app/recipe/recipe.model.ts
@@ -2,7 +2,7 @@ export class Recipe{
     public name: string ;
     public description: string;
     public imagePath: string;
-    constructor(name:string , desc:string , imagePath:string)
+    constructor(name:string ='A', desc:string='B' , imagePath:string='C')
     {
         this.name = name;
         this.description = desc;
