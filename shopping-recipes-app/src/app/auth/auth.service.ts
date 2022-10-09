b192a21e2e5641c92397658ab2c886c150a15817
mohamed-samy2499 <1701198@eng.asu.edu.eg>
2022-10-09 14:54:36 +0200

    saving user data when login or signup

--- shopping-recipes-app/src/app/auth/auth.service.ts
+++ shopping-recipes-app/src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse, HttpResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, Observable, throwError } from "rxjs";
+import { catchError, Observable, Subject, tap, throwError } from "rxjs";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
     "idToken": string, "email": string, "refreshToken": string,
@@ -9,6 +10,7 @@ export interface AuthResponseData {
 @Injectable({ providedIn: 'root' })
 
 export class AuthService {
+    user = new Subject<User>();
     private apiKey = "AIzaSyDgLHtQCQfNcDaPXFa6Yh1X3rC7_c23D0E";
 
     constructor(private http: HttpClient) { }
@@ -18,7 +20,8 @@ export class AuthService {
         const credentials = { 'email': email, 'password': password, 'returnSecureToken': true };
         return this.http.post<AuthResponseData>
             ('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.apiKey,
-                credentials).pipe(catchError(this.HandleError));
+                credentials).pipe(catchError(this.HandleError),
+                tap(this.HandleUserData));
     }
 
 
@@ -27,7 +30,8 @@ export class AuthService {
         return this.http.post<AuthResponseData>
             ('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='
              + this.apiKey,credentials)
-            .pipe(catchError(this.HandleError));
+            .pipe(catchError(this.HandleError),
+            tap(this.HandleUserData));
     }
 
 
@@ -40,14 +44,26 @@ export class AuthService {
                     }
                     switch (errorRes.error.error.message) {
                         case 'EMAIL_NOT_FOUND':
-                        errorMsg = "this user doesn't exists!";
-                        break;
+                            errorMsg = "this user doesn't exists!";
+                            break;
                         case 'EMAIL_EXISTS':
-                        errorMsg = "this user already exists!";
-                        break;
+                            errorMsg = "this user already exists!";
+                            break;
+                        case 'INVALID_PASSWORD':
+                            errorMsg = "this password is invalid!";
+                            break;
                     }
                         // throw new Error(errorMsg);
                         console.log(errorRes);
                     return throwError( ()=> errorMsg );
+    }
+
+    private HandleUserData(response: AuthResponseData){
+        const expirationDate = new Date(new Date().getTime() + +response.expiresIn);
+        const user = new User(response.email,
+            response.localId,
+            response.idToken,
+            expirationDate);
+        this.user.next(user);
     }
 }
