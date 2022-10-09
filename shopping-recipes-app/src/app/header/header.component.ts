d3369ae30dcc40ce077f96dbc6f8daa83ddcbf45
mohamed-samy2499 <1701198@eng.asu.edu.eg>
2022-10-09 15:53:33 +0200

    change the navbar based on the authentication

--- shopping-recipes-app/src/app/header/header.component.ts
+++ shopping-recipes-app/src/app/header/header.component.ts
@@ -1,22 +1,32 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor(private dataStorageService: DataStorageService) { }
+  isAuthenticated:boolean = false;
+  userSub?:Subscription;
 
+  constructor(private dataStorageService: DataStorageService,private authService:AuthService) { }
+
+  ngOnInit(){
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !user? false : true;
+    });
+  }
   onSaveData(){
     this.dataStorageService.storeRecipes();
   }
   onFetchData(){
     this.dataStorageService.fetchRecipes().subscribe();
   }
-  ngOnInit(): void {
+  ngOnDestroy(){
+      this.userSub?.unsubscribe();
   }
-
 }
