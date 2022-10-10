2e79f020da3905b88d75bb366c4ab716821bb0c6
mohamed-samy2499 <1701198@eng.asu.edu.eg>
2022-10-10 19:14:01 +0200

    adding AuthModule as another featureModule

--- shopping-recipes-app/src/app/app.module.ts
+++ shopping-recipes-app/src/app/app.module.ts
@@ -1,36 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+
+import { recipesModule } from './recipe/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeService } from './recipe/recipe.service';
-import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { recipesModule } from './recipe/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
- 
-    AuthComponent
-
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     recipesModule,
     ShoppingListModule,
     SharedModule,
+    AuthModule,
     CoreModule
   ],
   bootstrap: [AppComponent]
